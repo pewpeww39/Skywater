86fdb50f2c9535ceb42856ac9b557dada25bf56a
Jacob <jacobpew@outlook.com>
2021-10-15 15:31:54 -0400

    file

--- SMU_scripts/.ipynb_checkpoints/readVoltage_test-checkpoint.py
+++ SMU_scripts/.ipynb_checkpoints/readVoltage_test-checkpoint.py
@@ -1,15 +1,15 @@
 import pytest
 from keithley2600 import Keithley2600, ResultTable
 
-k = Keithley2600('TCPIP::169.254.0.1::INSTR')
-r1=1500
+k = Keithley2600('TCPIP2::169.254.0.1::INSTR')
+r1=1800
 def voltage_measure(y):
-    apply_voltage(k.smub, y)
+    k.apply_voltage(k.smua, y)
     x = k.measure_current(k.smua) * r1
     return x #k.measure_voltage(k.smua, input)
     
 def test_voltage_measure():
-    assert voltage_measure(10) == apporx(10)
+    assert voltage_measure(10) == pytest.approx(10)
 
 # def capital_case(x):
 #     return x
